d60278bf108a5b5cf4b35cc457dde128e1c44d36
Nguyen Thanh Dat <datntpk00530.fpt.edu.vn>
2016-03-08 15:27:01 +0700

    tai du lieu len github

---
+++ WindowsApplication2/WindowsApplication2/RrmQLNhaTro.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RrmQLNhaTro
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbnguoithuetro = New System.Windows.Forms.Label()
+        Me.lbgioitinh = New System.Windows.Forms.Label()
+        Me.lbdiachi = New System.Windows.Forms.Label()
+        Me.lbsdt = New System.Windows.Forms.Label()
+        Me.lbcmnd = New System.Windows.Forms.Label()
+        Me.txtnguoithuetro = New System.Windows.Forms.TextBox()
+        Me.txtgioitinh = New System.Windows.Forms.TextBox()
+        Me.txtdiachi = New System.Windows.Forms.TextBox()
+        Me.txtcmnd = New System.Windows.Forms.TextBox()
+        Me.txtsdt = New System.Windows.Forms.TextBox()
+        Me.btthoat = New System.Windows.Forms.Button()
+        Me.btxoa = New System.Windows.Forms.Button()
+        Me.btsua = New System.Windows.Forms.Button()
+        Me.btthem = New System.Windows.Forms.Button()
+        Me.lwquanlinhatro = New System.Windows.Forms.ListView()
+        Me.lbsophongtro = New System.Windows.Forms.Label()
+        Me.txtsophongtro = New System.Windows.Forms.TextBox()
+        Me.ColumnHeader7 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader8 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader9 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader10 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader11 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'lbnguoithuetro
+        '
+        Me.lbnguoithuetro.AutoSize = True
+        Me.lbnguoithuetro.Location = New System.Drawing.Point(72, 104)
+        Me.lbnguoithuetro.Name = "lbnguoithuetro"
+        Me.lbnguoithuetro.Size = New System.Drawing.Size(125, 17)
+        Me.lbnguoithuetro.TabIndex = 0
+        Me.lbnguoithuetro.Text = "Tên người thuê trọ"
+        '
+        'lbgioitinh
+        '
+        Me.lbgioitinh.AutoSize = True
+        Me.lbgioitinh.Location = New System.Drawing.Point(72, 157)
+        Me.lbgioitinh.Name = "lbgioitinh"
+        Me.lbgioitinh.Size = New System.Drawing.Size(60, 17)
+        Me.lbgioitinh.TabIndex = 0
+        Me.lbgioitinh.Text = "Giới tính"
+        '
+        'lbdiachi
+        '
+        Me.lbdiachi.AutoSize = True
+        Me.lbdiachi.Location = New System.Drawing.Point(547, 152)
+        Me.lbdiachi.Name = "lbdiachi"
+        Me.lbdiachi.Size = New System.Drawing.Size(51, 17)
+        Me.lbdiachi.TabIndex = 0
+        Me.lbdiachi.Text = "Địa chỉ"
+        '
+        'lbsdt
+        '
+        Me.lbsdt.AutoSize = True
+        Me.lbsdt.Location = New System.Drawing.Point(547, 104)
+        Me.lbsdt.Name = "lbsdt"
+        Me.lbsdt.Size = New System.Drawing.Size(36, 17)
+        Me.lbsdt.TabIndex = 0
+        Me.lbsdt.Text = "SĐT"
+        '
+        'lbcmnd
+        '
+        Me.lbcmnd.AutoSize = True
+        Me.lbcmnd.Location = New System.Drawing.Point(547, 60)
+        Me.lbcmnd.Name = "lbcmnd"
+        Me.lbcmnd.Size = New System.Drawing.Size(48, 17)
+        Me.lbcmnd.TabIndex = 0
+        Me.lbcmnd.Text = "CMND"
+        '
+        'txtnguoithuetro
+        '
+        Me.txtnguoithuetro.Location = New System.Drawing.Point(231, 99)
+        Me.txtnguoithuetro.Name = "txtnguoithuetro"
+        Me.txtnguoithuetro.Size = New System.Drawing.Size(237, 22)
+        Me.txtnguoithuetro.TabIndex = 1
+        '
+        'txtgioitinh
+        '
+        Me.txtgioitinh.Location = New System.Drawing.Point(231, 147)
+        Me.txtgioitinh.Name = "txtgioitinh"
+        Me.txtgioitinh.Size = New System.Drawing.Size(237, 22)
+        Me.txtgioitinh.TabIndex = 1
+        '
+        'txtdiachi
+        '
+        Me.txtdiachi.Location = New System.Drawing.Point(634, 152)
+        Me.txtdiachi.Name = "txtdiachi"
+        Me.txtdiachi.Size = New System.Drawing.Size(254, 22)
+        Me.txtdiachi.TabIndex = 1
+        '
+        'txtcmnd
+        '
+        Me.txtcmnd.Location = New System.Drawing.Point(634, 55)
+        Me.txtcmnd.Name = "txtcmnd"
+        Me.txtcmnd.Size = New System.Drawing.Size(254, 22)
+        Me.txtcmnd.TabIndex = 1
+        '
+        'txtsdt
+        '
+        Me.txtsdt.Location = New System.Drawing.Point(634, 99)
+        Me.txtsdt.Name = "txtsdt"
+        Me.txtsdt.Size = New System.Drawing.Size(254, 22)
+        Me.txtsdt.TabIndex = 1
+        '
+        'btthoat
+        '
+        Me.btthoat.Location = New System.Drawing.Point(728, 222)
+        Me.btthoat.Name = "btthoat"
+        Me.btthoat.Size = New System.Drawing.Size(75, 23)
+        Me.btthoat.TabIndex = 2
+        Me.btthoat.Text = "Thoát"
+        Me.btthoat.UseVisualStyleBackColor = True
+        '
+        'btxoa
+        '
+        Me.btxoa.Location = New System.Drawing.Point(329, 222)
+        Me.btxoa.Name = "btxoa"
+        Me.btxoa.Size = New System.Drawing.Size(75, 23)
+        Me.btxoa.TabIndex = 2
+        Me.btxoa.Text = "Xóa"
+        Me.btxoa.UseVisualStyleBackColor = True
+        '
+        'btsua
+        '
+        Me.btsua.Location = New System.Drawing.Point(520, 222)
+        Me.btsua.Name = "btsua"
+        Me.btsua.Size = New System.Drawing.Size(75, 23)
+        Me.btsua.TabIndex = 2
+        Me.btsua.Text = "Sửa"
+        Me.btsua.UseVisualStyleBackColor = True
+        '
+        'btthem
+        '
+        Me.btthem.Location = New System.Drawing.Point(132, 222)
+        Me.btthem.Name = "btthem"
+        Me.btthem.Size = New System.Drawing.Size(75, 23)
+        Me.btthem.TabIndex = 2
+        Me.btthem.Text = "Thêm"
+        Me.btthem.UseVisualStyleBackColor = True
+        '
+        'lwquanlinhatro
+        '
+        Me.lwquanlinhatro.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader7, Me.ColumnHeader8, Me.ColumnHeader9, Me.ColumnHeader10, Me.ColumnHeader11, Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.lwquanlinhatro.FullRowSelect = True
+        Me.lwquanlinhatro.GridLines = True
+        Me.lwquanlinhatro.Location = New System.Drawing.Point(116, 281)
+        Me.lwquanlinhatro.Name = "lwquanlinhatro"
+        Me.lwquanlinhatro.Size = New System.Drawing.Size(754, 251)
+        Me.lwquanlinhatro.TabIndex = 3
+        Me.lwquanlinhatro.UseCompatibleStateImageBehavior = False
+        Me.lwquanlinhatro.View = System.Windows.Forms.View.Details
+        '
+        'lbsophongtro
+        '
+        Me.lbsophongtro.AutoSize = True
+        Me.lbsophongtro.Location = New System.Drawing.Point(72, 55)
+        Me.lbsophongtro.Name = "lbsophongtro"
+        Me.lbsophongtro.Size = New System.Drawing.Size(90, 17)
+        Me.lbsophongtro.TabIndex = 4
+        Me.lbsophongtro.Text = "Số phòng trọ"
+        '
+        'txtsophongtro
+        '
+        Me.txtsophongtro.Location = New System.Drawing.Point(231, 52)
+        Me.txtsophongtro.Name = "txtsophongtro"
+        Me.txtsophongtro.Size = New System.Drawing.Size(237, 22)
+        Me.txtsophongtro.TabIndex = 5
+        '
+        'ColumnHeader7
+        '
+        Me.ColumnHeader7.Text = "STT"
+        '
+        'ColumnHeader8
+        '
+        Me.ColumnHeader8.Text = "Số phòng"
+        Me.ColumnHeader8.Width = 86
+        '
+        'ColumnHeader9
+        '
+        Me.ColumnHeader9.Text = "Tên người thuê trọ"
+        Me.ColumnHeader9.Width = 151
+        '
+        'ColumnHeader10
+        '
+        Me.ColumnHeader10.Text = "Giới tính"
+        Me.ColumnHeader10.Width = 82
+        '
+        'ColumnHeader11
+        '
+        Me.ColumnHeader11.Text = "CMND"
+        Me.ColumnHeader11.Width = 80
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "SĐT"
+        Me.ColumnHeader1.Width = 116
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Địa chỉ"
+        Me.ColumnHeader2.Width = 152
+        '
+        'RrmQLNhaTro
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(933, 560)
+        Me.Controls.Add(Me.txtsophongtro)
+        Me.Controls.Add(Me.lbsophongtro)
+        Me.Controls.Add(Me.lwquanlinhatro)
+        Me.Controls.Add(Me.btthem)
+        Me.Controls.Add(Me.btsua)
+        Me.Controls.Add(Me.btxoa)
+        Me.Controls.Add(Me.btthoat)
+        Me.Controls.Add(Me.txtdiachi)
+        Me.Controls.Add(Me.txtcmnd)
+        Me.Controls.Add(Me.txtsdt)
+        Me.Controls.Add(Me.txtgioitinh)
+        Me.Controls.Add(Me.txtnguoithuetro)
+        Me.Controls.Add(Me.lbcmnd)
+        Me.Controls.Add(Me.lbsdt)
+        Me.Controls.Add(Me.lbdiachi)
+        Me.Controls.Add(Me.lbgioitinh)
+        Me.Controls.Add(Me.lbnguoithuetro)
+        Me.Name = "RrmQLNhaTro"
+        Me.Text = "QuanLyNhaTro"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbnguoithuetro As System.Windows.Forms.Label
+    Friend WithEvents lbgioitinh As System.Windows.Forms.Label
+    Friend WithEvents lbdiachi As System.Windows.Forms.Label
+    Friend WithEvents lbsdt As System.Windows.Forms.Label
+    Friend WithEvents lbcmnd As System.Windows.Forms.Label
+    Friend WithEvents txtnguoithuetro As System.Windows.Forms.TextBox
+    Friend WithEvents txtgioitinh As System.Windows.Forms.TextBox
+    Friend WithEvents txtdiachi As System.Windows.Forms.TextBox
+    Friend WithEvents txtcmnd As System.Windows.Forms.TextBox
+    Friend WithEvents txtsdt As System.Windows.Forms.TextBox
+    Friend WithEvents btthoat As System.Windows.Forms.Button
+    Friend WithEvents btxoa As System.Windows.Forms.Button
+    Friend WithEvents btsua As System.Windows.Forms.Button
+    Friend WithEvents btthem As System.Windows.Forms.Button
+    Friend WithEvents lwquanlinhatro As System.Windows.Forms.ListView
+    Friend WithEvents lbsophongtro As System.Windows.Forms.Label
+    Friend WithEvents txtsophongtro As System.Windows.Forms.TextBox
+    Friend WithEvents ColumnHeader7 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader8 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader9 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader10 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader11 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ WindowsApplication2/WindowsApplication2/RrmQLNhaTro.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Public Class RrmQLNhaTro
+    Dim sqlConnect As New QLNhatro
+    Private Sub RrmQLNhaTro_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadQLNhaTro()
+    End Sub
+
+    Private Sub LoadQLNhaTro()
+        lwquanlinhatro.Items.Clear()
+        Dim dt As DataTable = sqlConnect.GetDataTable("Select * from Danhsachthuetro")
+
+        For i As Integer = 0 To dt.Rows.Count - 1 Step 1
+            Dim item As New ListViewItem(lwquanlinhatro.Items.Count + 1)
+            item.SubItems.Add(dt.Rows(i).ItemArray(1))
+            item.SubItems.Add(dt.Rows(i).ItemArray(2))
+            item.SubItems.Add(dt.Rows(i).ItemArray(3))
+            item.SubItems.Add(dt.Rows(i).ItemArray(4))
+            item.SubItems.Add(dt.Rows(i).ItemArray(5))
+            item.SubItems.Add(dt.Rows(i).ItemArray(6))
+            lwquanlinhatro.Items.Add(item)
+        Next
+
+    End Sub
+    Private Sub BtThem_Click(sender As Object, e As EventArgs) Handles BtThem.Click
+        
+        sqlConnect.ExecuteNoneQuery("Insert into Danhsachthuetro(sophongtro,tennguoithuetro,GioiTinh,cmnd,sdt,DiaChi) values('" + txtsophongtro.Text + "',N'" + txtnguoithuetro.Text + "','" + txtgioitinh.Text + "','" + txtcmnd.Text + "',N'" + txtsdt.Text + "',N'" + txtdiachi.Text + "')")
+        LoadQLNhaTro()
+
+    End Sub
+
+    Private Sub BtThoat_Click(sender As Object, e As EventArgs) Handles BtThoat.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub BtSua_Click(sender As Object, e As EventArgs) Handles BtSua.Click
+      
+        For i As Integer = lwquanlinhatro.SelectedIndices.Count - 1 To 0 Step -1
+            Dim viTriCanSua As Integer = lwquanlinhatro.SelectedIndices(i)
+            Dim tennguoiCanSua As String = lwquanlinhatro.Items(viTriCanSua).SubItems(1).Text
+            sqlConnect.ExecuteNoneQuery("Update Danhsachthuetro set sophongtro='" + txtsophongtro.Text + "',tennguoithuetro=N'" + txtnguoithuetro.Text + "',GioiTinh='" + txtgioitinh.Text + "',cmnd=N'" + txtcmnd.Text + "',sdt='" + txtsdt.Text + "',diachi=N'" + txtdiachi.Text + "' where sophongtro=" + tennguoiCanSua)
+        Next
+        LoadQLNhaTro()
+
+    End Sub
+
+    Private Sub BtXoa_Click(sender As Object, e As EventArgs) Handles BtXoa.Click
+        For i As Integer = lwquanlinhatro.SelectedIndices.Count - 1 To 0 Step -1
+            Dim viTriCanXoa As Integer = lwquanlinhatro.SelectedIndices(i)
+            Dim tenngnuoiCanXoa As String = lwquanlinhatro.Items(viTriCanXoa).SubItems(1).Text
+            sqlConnect.ExecuteNoneQuery("Delete from danhsachthuetro where sophongtro =" + tenngnuoiCanXoa)
+        Next
+        LoadQLNhaTro()
+    End Sub
+
+   
+    Private Sub lwquanlinhatro_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lwquanlinhatro.SelectedIndexChanged
+        For Each ds As ListViewItem In lwquanlinhatro.SelectedItems
+            txtsophongtro.Text = ds.SubItems(1).Text
+            txtnguoithuetro.Text = ds.SubItems(2).Text
+            txtgioitinh.Text = ds.SubItems(3).Text
+            txtcmnd.Text = ds.SubItems(4).Text
+            txtsdt.Text = ds.SubItems(5).Text
+            txtdiachi.Text = ds.SubItems(6).Text
+
+        Next
+    End Sub
+End Class
+
